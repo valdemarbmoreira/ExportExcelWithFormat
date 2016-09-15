5f0fdfb22a702d0e5d15a3a74e84658dd9b215d5
valdemarbmoreira <valdemarbmoreira@gmail.com>
2016-09-14 23:59:20 -0300

    01 - initialize project

---
+++ ProjectDownloadExcel/Main.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="ProjectDownloadExcel.Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:button ID="btnGerar" runat="server" text="Button" OnClick="btnGerar_Click" />
+    </div>
+    </form>
+    <div id="areaCheckBox">
+        
+
+    </div>
+</body>
+</html>
+
---
+++ ProjectDownloadExcel/Main.aspx.cs
@@ -0,0 +1,66 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectDownloadExcel
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGerar_Click(object sender, EventArgs e)
+        {
+            var data = new[]{ 
+                               new{ Name="Ram", Email="ram@techbrij.com safaadfafafadsfas sdfdsf sfdasfs asdfsfa", Phone="111-222-3333" },
+                               new{ Name="ShyamÇÇÇÇ", Email="shyam@techbrij.com", Phone="159-222-1596" },
+                               new{ Name="Mohan", Email="mohan@techbrij.com", Phone="456-222-4569" },
+                               new{ Name="Sohan", Email="sohan@techbrij.com", Phone="789-456-3333" },
+                               new{ Name="Karan", Email="karan@techbrij.com", Phone="111-222-1234" },
+                               new{ Name="Brij", Email="brij@techbrij.com", Phone="111-222-3333" }                       
+                      };
+
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Sony");
+            workSheet.Cells[1, 1].LoadFromCollection(data, true);
+            workSheet.Cells[1, 1, 1, 3].Style.Font.Bold = true;
+            workSheet.Cells[1, 1, 1, 3].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                                var fill = workSheet.Cells[1, 1, 1, 3].Style.Fill;
+                    fill.PatternType = ExcelFillStyle.Solid;
+                    fill.BackgroundColor.SetColor(Color.Gray);
+
+                    var border = workSheet.Cells[1, 1, 1, 3].Style.Border;
+                    border.Bottom.Style = ExcelBorderStyle.Thin;
+                    border.Top.Style = ExcelBorderStyle.Thin;
+                    border.Left.Style = ExcelBorderStyle.Thin;
+                    border.Right.Style = ExcelBorderStyle.Thin;
+
+                    //workSheet.Cells.AutoFitColumns();
+                    workSheet.Column(2).Width = 50;
+                    workSheet.Column(2).Style.WrapText = true;
+                    workSheet.Column(2).Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+
+            //workSheet.Cells[1, 1].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            //workSheet.Cells[1,1].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.Green);
+            using (var memoryStream = new MemoryStream())
+            {
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;  filename=Contact.xlsx");
+                excel.SaveAs(memoryStream);
+                memoryStream.WriteTo(Response.OutputStream);
+                Response.Flush();
+                Response.End();
+            }
+        }
+    }
+}
